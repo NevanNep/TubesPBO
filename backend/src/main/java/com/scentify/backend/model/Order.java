e95bf894dde8e9baa88a3aeaafd53da1f11a2879
unknown <velisianr@gmail.com>
2025-05-18 15:51:13 +0700

    Bikin class order

--- backend/src/main/java/com/scentify/backend/model/Order.java
+++ backend/src/main/java/com/scentify/backend/model/Order.java
@@ -1,5 +1,15 @@
 package com.scentify.backend.model;
 
 public class Order {
+    private String orderId;
+    private Buyer buyer;
+    private String status;
+    private PaymentMethod payMethod;
     
+    public void processPayment(){
+
+    }
+    public generateInvoice(){
+        
+    }
 }

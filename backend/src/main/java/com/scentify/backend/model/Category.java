be2593603565f7c779c91524f7c0c3ffd367e550
velinihan <velisianr@gmail.com>
2025-05-22 10:57:03 +0700

    Create Category.java

---
+++ backend/src/main/java/com/scentify/backend/model/Category.java
@@ -0,0 +1,13 @@
+package com.scentify.backend.model;
+
+import java.util.List;
+
+public class Category {
+    private String categoryId;
+    private String nama;
+    private List<Product> products;
+
+    public void addProduct() {
+
+    }
+}

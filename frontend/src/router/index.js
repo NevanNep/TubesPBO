7df6b7ec32a7a2da330d33c8d6d653540448ca3a
Jeanyferliza <jeanyferlizanayla@gmail.com>
2025-05-22 12:44:33 +0700

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MenuPage from '../components/MenuPage.vue'; // Halaman utama menu produk
 import ProductDetail from '../components/ProductDetail.vue'; // Halaman detail produk
 import CartPage from '../components/CartPage.vue'; // Halaman keranjang
-import PaymentPage from '../components/Paymentpage.vue'; // Halaman pembayaran
+import PaymentPage from '../components/PaymentPage.vue'; // Halaman pembayaran
 import LoginPage from '../components/LoginPage.vue'; // Halaman login
 import RegisterPage from '../components/RegisterPage.vue'; // Halaman register
 import ReviewPage from '@/components/ReviewPage.vue';

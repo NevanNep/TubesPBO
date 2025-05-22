f780eb62037f77d6d61fe27ca70d07fa2d4fa2b6
Jeanyferliza <jeanyferlizanayla@gmail.com>
2025-05-22 12:42:33 +0700

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Header -->
+    <header class="bg-red-700 text-white py-3">
+      <div class="container d-flex justify-content-between align-items-center">
+        <h1 class="h3">SCENTIFY</h1>
+        <!-- Dropdown Kategori Parfum -->
+        <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Parfum Pria</a></li>
+    <li><a class="dropdown-item" href="#">Parfum Wanita action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+        <div>
+          <router-link to="/login" class="text-white mx-3">Login</router-link>
+          <router-link to="/register" class="text-white mx-3">Register</router-link>
+        </div>
+      </div>
+    </header>
+
+    <!-- Konten Halaman -->
+    <router-view></router-view>
+
+    <!-- Footer -->
+    <footer class="bg-red-700 text-white py-4 mt-5">
+      <div class="container text-center">
+        <p>&copy; 2025 SCENTIFY - All Rights Reserved</p>
+        <div>
+          <a href="#" class="text-white mx-3">Contact</a>
+          <a href="#" class="text-white mx-3">About</a>
+          <a href="#" class="text-white mx-3">Terms</a>
+        </div>
+        <div class="mt-2">
+          <a href="#" class="text-white mx-3"><i class="bi bi-facebook"></i> Facebook</a>
+          <a href="#" class="text-white mx-3"><i class="bi bi-whatsapp"></i> WhatsApp</a>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.bg-red-700 {
+  background-color: #66bcf1;
+}
+
+.text-white {
+  color: white;
+}
+
+.text-white:hover {
+  color: #151010;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
 }
 </style>
